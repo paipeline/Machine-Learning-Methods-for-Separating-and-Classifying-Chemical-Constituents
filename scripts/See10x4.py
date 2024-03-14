2ed53bf00f43689213768f65b32c6f3a6b3159d2
cli648 <cli648@wisc.edu>
2024-03-14 10:08:00 -0500

    Script in ./script/See10x4.py result in ./figs/Comaprison RAW SLICED BASELINE_REMOVED NORMALIZED.png

---
+++ scripts/See10x4.py
@@ -0,0 +1,101 @@
+# This file is used to show 10*4 comparison with raw, sliced from 350 - 2000, baselined_removed, and normalized
+# not a Jupytor notebook because I don't have PyCharm Professional :(
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+from src.preprocessor import Preprocessor
+from BaselineRemoval import BaselineRemoval
+
+#RAW
+
+# Since we want 10 different cols, I use 10 different files
+
+raw_data = []
+dir = '../raw_dataset' # change this as needed
+
+#print(os.listdir('../raw_dataset'))
+files = ['SERS 1BaP+2ANTH.xlsx', 'SERS 2ANTH+1PYR.xlsx', 'SERS 1BaP+1PYR.xlsx', 'ImidaclopridTapwaterdata.xlsx',
+         'SERS 1ANTH+1PYR+1BaP+2BaA.xlsx', 'SERS 1BaP+2BaA.xlsx', 'SERS 1ANTH+2PYR+1BaP+1BaA.xlsx',
+         'SERS 5ANTH+1PYR.xlsx', 'SERS 2ANTH+1PYR+1BaP+1BaA.xlsx', 'SERS BaA.xlsx']
+for file in os.listdir(dir):
+    if file in files:
+        file_path = os.path.join(dir, file)
+        processor = Preprocessor(file_path,m=10, threshold=20,mat_iteration=100,window=50, prominence=0.02)
+        raw_x, raw_y = processor.load_raw_data(file_path)
+        if file == 'ImidaclopridTapwaterdata.xlsx':
+            single_raw_y = raw_y[0]
+        else:
+            single_raw_y = raw_y[5]
+        raw_data.append([raw_x, single_raw_y])
+
+# Ploting
+plt.figure(figsize=(100, 50)) # Adjust this figsize as needed
+plt.suptitle('Raw / Sliced / BaselineRemoved / Normalized')
+
+for i, (raw_x, raw_y) in enumerate(raw_data, start=1):
+    plt.subplot(10, 4, 4*i - 3)
+    plt.plot(raw_x, raw_y)
+    plt.title(files[i-1])
+    plt.xlabel('xVal')
+    plt.ylabel('yVal')
+    plt.grid(True)
+
+
+
+# Now is slicing
+
+sliced_data = []
+for (xval, yval) in raw_data:
+    sliced = [(x, y) for x, y in zip(xval, yval) if 350 <= x <= 2000]
+    sliced_x, sliced_y = zip(*sliced)
+    sliced_data.append([sliced_x, sliced_y])
+
+# Plot
+for i, (sliced_x, sliced_y) in enumerate(sliced_data, start=1):
+    plt.subplot(10, 4, 4*i-2)
+    plt.plot(sliced_x, sliced_y)
+    plt.title(files[i-1])
+    plt.xlabel('xVal')
+    plt.ylabel('yVal')
+    plt.grid(True)
+
+
+# Basline Removal:
+baseline_removed_data = []
+for val in sliced_data:
+    x, y = val
+    baseObj = BaselineRemoval(y)
+    baseline_removed_y = baseObj.ZhangFit()
+    baseline_removed_data.append([x, baseline_removed_y])
+
+for i, (x, y) in enumerate(baseline_removed_data, start=1):
+    plt.subplot(10, 4, 4*i - 1)
+    plt.plot(x, y)
+    plt.title(files[i-1])
+    plt.xlabel('xVal')
+    plt.ylabel('yVal')
+    plt.grid(True)
+
+# Normalization
+
+normalized_val = []
+for val in baseline_removed_data:
+    x, y = val
+    normalized_y = (y - min(y)) / (max(y) - min(y))
+    normalized_val.append([x, normalized_y])
+
+for i, (x, y) in enumerate(normalized_val, start=1):
+    plt.subplot(10, 4, 4*i)
+    plt.plot(x, y)
+    plt.title(files[i-1])
+    plt.xlabel('xVal')
+    plt.ylabel('yVal')
+    plt.grid(True)
+
+plt.tight_layout(rect=[0, 0, 1, 0.98])
+plt.savefig('../fig/Comparison RAW SLICED BASELINE_REMOVED NORMALIZED')
+plt.show()
+
+
+
+
